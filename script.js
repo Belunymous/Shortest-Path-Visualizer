52527dc262c50868119fd4ab863907ac3a5239b2
Ashutosh Jha <ashutosh14.ak@gmail.com>
2024-08-01 16:34:53 +0530

    Add Edges

---
+++ script.js
@@ -0,0 +1,27 @@
+let blocks = document.getElementsByClassName("drawing-area")[0];
+let addEdge = false;
+let cnt = 0;
+let dist;
+
+let alerted = localStorage.getItem("alerted") || "";
+if (alerted !== "yes") {
+  alert(
+    "Read instructions before proceeding by clicking i-icon in the top-right corner"
+  );
+  localStorage.setItem("alerted", "yes");
+}
+
+const addEdges = () => {
+  if (cnt < 2) {
+    alert("Create atleast two nodes to add an edge");
+    return;
+  }
+
+  addEdge = true;
+  document.getElementById("add-edge-enable").disabled = true;
+  document.getElementsByClassName("run-btn")[0].disabled = false;
+  // Initializing array for adjacency matrix representation
+  dist = new Array(cnt + 1)
+    .fill(Infinity)
+    .map(() => new Array(cnt + 1).fill(Infinity));
+};
