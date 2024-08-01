681dc00c5dd7714ebface0375351373eadf8090f
Ashutosh Jha <ashutosh14.ak@gmail.com>
2024-08-01 16:35:44 +0530

    Add Nodes

--- script.js
+++ script.js
@@ -25,3 +25,49 @@ const addEdges = () => {
     .fill(Infinity)
     .map(() => new Array(cnt + 1).fill(Infinity));
 };
+
+let arr = [];
+
+const appendBlock = (x, y) => {
+  document.querySelector(".reset-btn").disabled = false;
+  document.querySelector(".click-instruction").style.display = "none";
+  // Creating a node
+  const block = document.createElement("div");
+  block.classList.add("block");
+  block.style.top = `${y}px`;
+  block.style.left = `${x}px`;
+  block.style.transform = `translate(-50%,-50%)`;
+  block.id = cnt;
+
+  block.innerText = cnt++;
+
+  // Click event for node
+  block.addEventListener("click", (e) => {
+    // Prevent node upon node
+    e.stopPropagation() || (window.Event.cancelBubble = "True");
+
+    // If state variable addEdge is false, can't start adding edges
+    if (!addEdge) return;
+
+    block.style.backgroundColor = "coral";
+    arr.push(block.id);
+
+    // When two elements are push, draw a edge and empty the array
+    if (arr.length === 2) {
+      drawUsingId(arr);
+      arr = [];
+    }
+  });
+  blocks.appendChild(block);
+};
+
+// Allow creating nodes on screen by clicking
+blocks.addEventListener("click", (e) => {
+  if (addEdge) return;
+  if (cnt > 15) {
+    alert("cannot add more than 15 vertices");
+    return;
+  }
+  console.log(e.x, e.y);
+  appendBlock(e.x, e.y);
+});
